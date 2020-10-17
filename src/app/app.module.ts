e754adc2dbf51abd5e353112f32f738288293e59
Fabrício Vasconcellos <fabricio.vasconc@gmail.com>
2020-10-16 21:03:07 -0300

    Added packages and create login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        CardModule,
+        InputTextModule,
+        ButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
